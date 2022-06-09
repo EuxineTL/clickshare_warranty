df2f58abc628c5df310667ae1eae7fb28bff36b0
euxinetl <euxinetl@gmail.com>
2022-06-09 07:35:09 -0400

    finalize the test plan

--- tests/test_warranty.py
+++ tests/test_warranty.py
@@ -2,10 +2,17 @@
 These tests cover BARCO ClickShare Warranty Info.
 """
 
-def test_warranty(browser):
+from pages.warranty_info import WarrantyPage
 
-    # ==Given== `Check your ClickShare warranty` page is displayed 
-    # ==When== User input `serial number` to get warranty info 
-    # ==Then== Warranty result for `serial number` is displayed 
-    # ==And== Verify `Warranty end date` exists in the warranty result 
-    return
+def test_warranty(browser):
+    warranty_page = WarrantyPage(browser)
+    serial_number = '1863552437'
+
+    # ==Given== `Check your ClickShare warranty` page is displayed
+    warranty_page.load()
+    # ==When== User input `serial number` to get warranty info
+    warranty_page.getinfo(serial_number)
+    # ==Then== Warranty result for `serial number` is displayed
+    assert warranty_page.resultheader() == serial_number
+    # ==And== Verify `Warranty end date` exists in the warranty result
+    assert warranty_page.resultwarranty('Warranty end date')==True
