14ec6f7b45c350aac5e06bb9dbc7211d5f124434
euxinetl <euxinetl@gmail.com>
2022-06-09 06:15:32 -0400

    test plan placeholder

---
+++ tests/test_warranty.py
@@ -0,0 +1,11 @@
+"""
+These tests cover BARCO ClickShare Warranty Info.
+"""
+
+def test_warranty(browser):
+
+    # ==Given== `Check your ClickShare warranty` page is displayed 
+    # ==When== User input `serial number` to get warranty info 
+    # ==Then== Warranty result for `serial number` is displayed 
+    # ==And== Verify `Warranty end date` exists in the warranty result 
+    return
